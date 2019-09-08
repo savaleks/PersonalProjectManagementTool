c13440934943705a865d493b2b97da8a9a5d644b
Alexander <savaleks@gmail.com>
2019-09-08 09:13:10 +0300

    added files

---
+++ PPMToolFullStack/src/main/java/com/savaleks/ppmtool/services/ProjectService.java
@@ -0,0 +1,21 @@
+package com.savaleks.ppmtool.services;
+
+import com.savaleks.ppmtool.domain.Project;
+import com.savaleks.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        //Logic
+
+        return projectRepository.save(project);
+    }
+
+}
