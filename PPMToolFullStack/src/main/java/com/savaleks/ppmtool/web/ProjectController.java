ebb08fa63e4de34dad8321029623814307f3fadb
Alexander <savaleks@gmail.com>
2019-09-08 09:46:39 +0300

    find all project

--- PPMToolFullStack/src/main/java/com/savaleks/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/savaleks/ppmtool/web/ProjectController.java
@@ -43,4 +43,7 @@ public class ProjectController {
 
         return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects(){return projectService.findAllProjects();}
 }
