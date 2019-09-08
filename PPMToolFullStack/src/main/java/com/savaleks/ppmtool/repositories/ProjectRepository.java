ef87a3cd30f26ce8211c6865ee56e5ea0d7adb0c
Alexander <savaleks@gmail.com>
2019-09-08 09:05:24 +0300

    added repo

---
+++ PPMToolFullStack/src/main/java/com/savaleks/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.savaleks.ppmtool.repositories;
+
+import com.savaleks.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
