4c16ca27df2922edd89e637e5fe91dd6f2a907e8
Alexander <savaleks@gmail.com>
2019-09-15 09:51:07 +0300

    project not found exception

--- PPMToolFullStack/src/main/java/com/savaleks/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/com/savaleks/ppmtool/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package com.savaleks.ppmtool.services;
 
 import com.savaleks.ppmtool.domain.Backlog;
+import com.savaleks.ppmtool.domain.Project;
 import com.savaleks.ppmtool.domain.ProjectTask;
+import com.savaleks.ppmtool.exceptions.ProjectNotFoundException;
 import com.savaleks.ppmtool.repositories.BacklogRepository;
+import com.savaleks.ppmtool.repositories.ProjectRepository;
 import com.savaleks.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,27 +21,39 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
-        // add task to specific project
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        projectTask.setBacklog(backlog);
-        Integer BacklogSequence = backlog.getPTSequence();
-        BacklogSequence++;
-        backlog.setPTSequence(BacklogSequence);
-        projectTask.setProjectSequence(backlog.getProjectIdentifier()+"-"+BacklogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
 
-        if (projectTask.getStatus()==""||projectTask.getStatus()==null){
-            projectTask.setStatus("TO_DO");
-        }
+        try {
+            // add task to specific project
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            projectTask.setBacklog(backlog);
+            Integer BacklogSequence = backlog.getPTSequence();
+            BacklogSequence++;
+            backlog.setPTSequence(BacklogSequence);
+            projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + BacklogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        if (projectTask.getPriority()==null){
-            projectTask.setPriority(3);
+            if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+                projectTask.setStatus("TO_DO");
+            }
+
+            if (projectTask.getPriority() == null) {
+                projectTask.setPriority(3);
+            }
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e){
+            throw new ProjectNotFoundException("Project not found.");
         }
-        return projectTaskRepository.save(projectTask);
     }
 
    public Iterable<ProjectTask> findBacklogById(String id){
+       Project project = projectRepository.findByProjectIdentifier(id);
+       if (project == null){
+           throw new ProjectNotFoundException("Project with id: " + id + " does not exist.");
+       }
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
    }
 }
