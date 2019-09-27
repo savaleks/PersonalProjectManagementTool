feb5d50ed1abfb2de7ec7e155e01d16be97c302a
Alexander <savaleks@gmail.com>
2019-09-27 09:59:54 +0300

    added register and login page

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -11,6 +11,9 @@ import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/projectBoard/ProjectBoard";
 import AddProjectTask from "./components/projectBoard/ProjectTasks/AddProjectTask";
 import UpdateProjectTask from "./components/projectBoard/ProjectTasks/UpdateProjectTask";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/UserManagement/Register";
+import Login from "./components/UserManagement/Login";
 
 function App() {
   return (
@@ -18,6 +21,15 @@ function App() {
       <Router>
         <div className="App">
           <Header />
+          {
+            // Public routes
+          }
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          {
+            // Private routes
+          }
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
